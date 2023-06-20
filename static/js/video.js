00725157f15ae08feeadcc1c0d01718183e10955
nicola <n.laurino1@studenti.unisa.it>
2023-06-20 18:49:35 +0200

    Subject: Pull Request - Adding Video Button Management and Webcam Snapshot Automation
    1. Video Button Management:
       - Implemented functionality to show/hide a button based on the video state (playing or paused).
    
    2. Webcam Snapshot Automation:
       - Added JavaScript code to automatically capture a webcam snapshot every 20 seconds.
       - The snapshots are sent to the server for further processing.
    
    3. Stop Automation:
       - Implemented a stop button that interrupts the automated webcam snapshot process when pressed.
    
    These changes enhance the user experience and add automation capabilities to the project. Kindly review the code and provide your feedback.
    
    #4

--- static/js/video.js
+++ static/js/video.js
@@ -1,6 +1,7 @@
 const video = document.getElementById('videoElement');
 const startButton = document.getElementById('startButton');
-
+const stopButton = document.getElementById('stopButton');
+var intervalID;
 // Opzioni per l'acquisizione video
 const constraints = { video: true };
 
@@ -13,6 +14,9 @@ function startVideo() {
     .catch((error) => {
       console.error('Errore nell\'acquisizione video:', error);
     });
+    startButton.classList.add('d-none');
+    stopButton.classList.remove('d-none');
+    intervalID = setInterval(takeSnapshot, 20000);
 }
 
 function takeSnapshot() {
@@ -29,7 +33,6 @@ function takeSnapshot() {
    const data = {
     image: imageDataURL
   };
-   console.log(data.toString() + "********************")
   fetch(url, {
         method: 'POST',
         headers: {
@@ -41,4 +44,10 @@ function takeSnapshot() {
     .then(response => response.json())
     .then(data => {console.log(data)})
     .catch(error => {console.error('Errore: ', error.toString())})
+}
+
+function stopVideo() {
+    clearInterval(intervalID);
+    startButton.classList.remove('d-none');
+    stopButton.classList.add('d-none');
 }
