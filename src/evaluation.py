f995c6977e9ca59a549bf64189071613267e5290
VincentMilione <milionevincent@gmail.com>
2023-06-21 10:17:23 +0200

    Added more metrics
    In this commit, I managed to add the classification report to the outputs of the evaluation phase. I also eliminated the batch attribute from the imagedatagenerator as it is not used

--- src/evaluation.py
+++ src/evaluation.py
@@ -10,7 +10,7 @@ seed = params["seed"]
 testDataGenerator = ImageDataGenerator(rescale=1./255)
 test_dir = "data/preprocessed/test"
 #batch size set to 64 because the whole set doesn't fit in memory
-test_data = testDataGenerator.flow_from_directory(test_dir, batch_size=64, target_size=(48, 48), shuffle=False, color_mode='grayscale', class_mode='categorical', seed=seed)
+test_data = testDataGenerator.flow_from_directory(test_dir, target_size=(48, 48), shuffle=False, color_mode='grayscale', class_mode='categorical', seed=seed)
 # Load the model
 with open('model/emotion_model.json', 'r') as json_file:
     loaded_model_json = json_file.read()
@@ -21,6 +21,7 @@ model.load_weights('model/emotion_model.h5')
 model.compile(loss='categorical_crossentropy', metrics=[f1_score])
 
 #Evaluate model
+print(test_data.n)
 predictions = model.predict_generator(test_data)
 predicted_labels = np.argmax(predictions, axis=1)
 
@@ -29,7 +30,7 @@ true_labels = test_data.classes
 class_labels = list(test_data.class_indices.keys())
 
 #extract overall metrics
-f1_score = f1_score(y_true=true_labels, y_pred=predicted_labels, average="macro")
+f1_score = f1_score(y_true=true_labels, y_pred=predicted_labels, average="micro")
 precision = precision_score(y_true=true_labels, y_pred=predicted_labels, average="macro")
 recall = recall_score(y_true=true_labels, y_pred=predicted_labels, average="macro")
 report = classification_report(y_true=true_labels, y_pred=predicted_labels, target_names=class_labels, output_dict=True)
@@ -42,3 +43,6 @@ with Live(resume=True) as live:
     live.summary['test']['f1_score'] = f1_score
 
     live.log_sklearn_plot("confusion_matrix", true_labels, predicted_labels)
+
+with Live(dir="dvc_extra_metrics") as live:
+    live.summary['test_report'] = report
