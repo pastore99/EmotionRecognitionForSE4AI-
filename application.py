cca8813941742576ad6fba94f81a7a6562437a96
nicola <n.laurino1@studenti.unisa.it>
2023-06-22 19:20:00 +0200

    Created history page, configured backend, and fixed frontend
    - Created a new history page to display historical data
    - Configured the backend to handle requests and retrieve historical data from the database
    - Fixed frontend issues related to the layout and styling of the history page

--- application.py
+++ application.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def main():
+def index():
     return render_template("index.html")
 
+@app.route('/storico')
+def storico():
+    return render_template('storico.html')
 
 if __name__ == "__main__":
     app.run()
